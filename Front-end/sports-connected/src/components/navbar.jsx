d416122458fdc30f5e90509e63d0346363a16ab4
Dominykas <dominykas.gecas@gmail.com>
2020-03-09 21:59:08 +0200

    Updated nav bar buttons
    Updated nav bar buttons
    Added home button

--- Front-end/sports-connected/src/components/navbar.jsx
+++ Front-end/sports-connected/src/components/navbar.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import LoginButton from "./login";
 import RegisterButton from "./register";
-import { Container, Navbar, Form } from "react-bootstrap";
+import { Container, Navbar, Form} from "react-bootstrap";
 
 class NavBar extends Component {
     render() {
@@ -9,11 +9,11 @@ class NavBar extends Component {
 
             <Navbar expand="lg" className="navbar-color">
                 <Container bsPrefix="container-fluid">
-                    <Navbar.Brand href="#" className="brand-name "> <p className="mb-0 text-bold text-light text-logo">SPORTS CONNECTED.</p></Navbar.Brand>
+                    <Navbar.Brand href="https://bit.ly/2TC2mrW" className="brand-name "> <p className="mb-0 text-bold text-light text-logo">SPORTS CONNECTED.</p></Navbar.Brand>   
                     <Form inline>
-                        <LoginButton />
-                        <RegisterButton />
-                    </Form>
+                        <LoginButton/>
+                        <RegisterButton/>
+                    </Form>   
                 </Container>
             </Navbar>
 
