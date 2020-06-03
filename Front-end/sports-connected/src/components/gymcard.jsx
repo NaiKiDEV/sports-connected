cef8defc8ac0d483f7b84ba7dfe860bb8df14260
Paulius <pc999111@gmail.com>
2020-06-03 19:35:19 +0300

    add gym offer modal menu

--- Front-end/sports-connected/src/components/gymcard.jsx
+++ Front-end/sports-connected/src/components/gymcard.jsx
@@ -1,12 +1,14 @@
 import React, { useState, useHistory, useEffect } from 'react';
-import { Container, Row, Col, Card, Modal, Form, Button } from 'react-bootstrap';
+import { Container, Row, Col, Card, Modal, Form, Button, Accordion } from 'react-bootstrap';
 import SingleGym from './singlegym';
 import SingleGymOffer from './singlegymoffer';
 
 function GymCard() {
 
     const [modalOpen, setmodalOpen] = useState(false);
+    const [gyms, setGyms] = useState([]);
     const [offers, setOffers] = useState([]);
+
 
     function openModal() {
         setmodalOpen(!modalOpen);
@@ -34,8 +36,31 @@ function GymCard() {
             });
     }
 
+    function GetAllGyms() {
+        fetch("https://sportsconnectedback.azurewebsites.net/api/gyms",
+            {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }
+        )
+            .then(res => res.json())
+            .then(a => {
+                if (a.error === false) {
+                    const { returnResult } = a;
+                    console.log(returnResult);
+                    setGyms(returnResult);
+                    //return returnResult.forEach(element => <SingleGym name={element.title} url={element.ImageUrl} status="Laukiama pavedimo" ></SingleGym>
+                } else {
+                    alert(a.message);
+                }
+            });
+    }
+
     useEffect(() => {
         GetAllOffers();
+        GetAllGyms();
     }, []);
 
     return <Container className="pr-0">
@@ -67,7 +92,7 @@ function GymCard() {
                         <Card className="text-light modal-header menu-item single-gym">
                             <Card.Body>
                                 <Card.Title className="text-center">
-                                <i class="fas fa-plus fa-4x btn-addgymusr" onClick={() => openModal()}></i>
+                                    <i class="fas fa-plus fa-4x btn-addgymusr" onClick={() => openModal()}></i>
                                 </Card.Title>
                                 <Container className="text-center">
                                     <Row className="mt-4">
@@ -81,37 +106,85 @@ function GymCard() {
                 </Row>
             </Card.Body>
         </Card>
-        <TakeOfferModal isOpen={modalOpen} onModalClick={() => openModal()} gymOffers={offers}></TakeOfferModal>
+        <TakeOfferModal isOpen={modalOpen} onModalClick={() => openModal()} gymOffers={offers} gymArray={gyms} ></TakeOfferModal>
     </Container >
 }
 
 
 function DisplayOffers(props) {
-    return props.offersArray.map(element =>
-        <div>
-            <Row className="pl-4 mb-0 w-100 offers mx-0 py-2">
-                <Col sm={8}>
-                    <h4 className="text-white">{element.title}</h4>
-                </Col>
-                <Col sm={4} className="">
-                    <h4 className="text-white">{element.price} €</h4>
-                </Col>
-                <Col sm={8} className="text-center offset-2">
-                    <h5 className="">{element.description}</h5>
-                </Col>
-            </Row>
-        </div>
+    var filteredArray = props.offersArray.filter(e => e.gymId === props.filterKey);
+    //console.log(filteredArray);
+    return filteredArray.map(element =>
+        <Card
+            //onClick={() => props.setKey(element.id)}
+            className="border-zero"
+        >
+            <Accordion.Collapse eventKey="0">
+                <Card.Body className="px-0 py-0">
+                    <Row className="pl-4 mb-0 w-100 offers_select mx-0 py-2">
+                        <Col sm={8}>
+                            <h6 className="text-white">{element.title}</h6>
+                        </Col>
+                        <Col sm={4} className="">
+                            <h6 className="text-white text-right">{element.price} €</h6>
+                        </Col>
+
+                    </Row>
+                </Card.Body>
+            </Accordion.Collapse>
+        </Card>
     );
+}
+
+function DisplayAllGymData(props) {
+    var randkey = 0;
+    console.log(props.gymArray);
+    return props.gymArray.map(element => {
+        var randkeyVal = randkey.toString();
+        randkey++;
+        return <Card onClick={() => props.setKey(element.id)} className="border-zero">
+            <Accordion.Collapse eventKey="0">
+                <Card.Body className="px-0 py-0">
+                    <Row className="px-4 mb-0 w-100 offers_select mx-0 py-2">
+                        <Col sm={4}>
+                            <h6 className="text-white">{element.title}</h6>
+                        </Col>
+                        <Col sm={4} className="">
+                            <h6 className="text-white text-center">{element.fullAdress}</h6>
+                        </Col>
+                        <Col sm={4} className="text-white text-right">
+                            <h6 className="">{element.offers.length} pasiūlymai</h6>
+                        </Col>
+                    </Row>
+                </Card.Body>
+            </Accordion.Collapse>
+        </Card>
+    }
+
+    );
+}
+
+function DisplayGyms(props) {
+    return <div>
+        <Accordion >
+            <Accordion.Toggle as={Card.Header} eventKey="0">
+                <h4 className="text-white">Salės</h4>
+            </Accordion.Toggle>
+            <DisplayAllGymData gymArray={props.gymArray} setKey={props.setKey}></DisplayAllGymData>
+        </Accordion>
+    </div>
 }
 
 
 function TakeOfferModal(props) {
-
+    const [selectedKey, setKey] = useState([]);
+    const [selectedOffer, setOffer] = useState([]);
     return (
         <Modal
             aria-labelledby="contained-modal-title-vcenter"
             centered
             show={props.isOpen}
+            size="lg"
             //show={true}
             //onSubmit={() => loginUser()}
             onHide={props.onModalClick}>
@@ -122,8 +195,16 @@ function TakeOfferModal(props) {
             <Modal.Body className="modal-content">
                 <Form>
                     <Row className="">
-                        <Col sm="12">
-                            <DisplayOffers offersArray={props.gymOffers}></DisplayOffers>
+                        <Col sm="12" className="px-0">
+                            <DisplayGyms gymArray={props.gymArray} setKey={setKey}></DisplayGyms>
+                        </Col>
+                        <Col sm="12" className="px-0">
+                            <Accordion >
+                                <Accordion.Toggle as={Card.Header} eventKey="0">
+                                    <h4 className="text-white">Pasiūlymai</h4>
+                                </Accordion.Toggle>
+                                <DisplayOffers offersArray={props.gymOffers} filterKey={selectedKey}></DisplayOffers>
+                            </Accordion>
                         </Col>
                     </Row>
                     <Row>
